6710225482bf679f540be06e26191a595a891498
santiago de hoyos <santiagodehoyoshernandez@.net>
2025-09-03 17:48:47 -0500

    se agrego operaciones.py

---
+++ operaciones.py
@@ -0,0 +1,13 @@
+print("hola esta es la  calculadora de santiago")
+print("esta es la operacion multiplicacion")
+n1=int(input("ingrese el primer numero: "))
+n2=int(input("ingrese el segundo numero: "))
+def multiplicacion(n1,n2):
+    return n1*n2
+print("el resultafo es: ", multiplicacion(n1,n2))
+print("esta es la operacion division")
+n1=int(input("ingrese el primer numero: "))
+n2=int(input("ingrese el segundo numero: "))
+def division(n1,n2):
+    return n1/n2
+print("el resultado es: ", division(n1,n2))
